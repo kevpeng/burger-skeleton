623a67e52755419abac51bd82a7782058203f9af
Kevin Peng <kevin.peng@richmond.edu>
2018-11-23 13:51:42 +0100

    Added to router.js; lang button photos

--- src/router.js
+++ src/router.js
@@ -2,19 +2,21 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Ordering from './views/Ordering.vue'
 
+
 Vue.use(Router)
 
 let router = new Router({
   routes: [
-    {
+   {
       path: '/',
       meta: {
         title: 'Kraft Burgers',
       },
       name: 'ordering',
       component: Ordering
-    },
-    {
+
+   },
+   {
       path: '/kitchen',
       meta: {
         title: 'Kitchen',
@@ -24,7 +26,17 @@ let router = new Router({
       // this generates a separate chunk (read.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "read" */ './views/Kitchen.vue')
-    }
+   },
+   // Added start route
+   {
+      path: '/start',
+         meta: {
+            title: 'Kraft Burgers',
+         },
+      name: 'start',
+      component: () => import(/* webpackChunkName: "read" */ './views/Start.vue')
+   }
+
   ]
 })
 
