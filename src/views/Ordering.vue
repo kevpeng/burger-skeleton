b0326dcc7c6de6f2780570faf388f80f71606a5f
Linoj <linoj_2299@hotmail.com>
2019-01-07 21:52:58 +0100

    The code to click outside to close the cart, has been commented out, so I works as before

--- src/views/Ordering.vue
+++ src/views/Ordering.vue
@@ -354,45 +354,45 @@ export default {
         // 6. gray out the buttons on footer
         // 7. (optional) add a red 'X' at top left to show you can exit
         document.getElementById("myCart").classList.toggle("show");
-    },
-    outside: function(e) {
-    	this.clickOutside += 1
-        console.log('clicked outside!')
-        document.getElementById("myCart").classList.toggle("show");
-      },
-      inside: function(e) {
-      this.clickInside += 1
-        console.log('clicked inside!')
-      }
-  },
-  directives: {
-    'click-outside': {
-      bind: function(el, binding, vNode) {
-        // Provided expression must evaluate to a function.
-        if (typeof binding.value !== 'function') {
-        	const compName = vNode.context.name
-          let warn = `[Vue-click-outside:] provided expression '${binding.expression}' is not a function, but has to be`
-          if (compName) { warn += `Found in component '${compName}'` }
-          console.warn(warn)
-        }
-        // Define Handler and cache it on the element
-        const bubble = binding.modifiers.bubble
-        const handler = (e) => {
-          if (bubble || (!el.contains(e.target) && el !== e.target)) {
-          	binding.value(e)
-          }
-        }
-        el.__vueClickOutside__ = handler
-        // add Event Listeners
-        document.addEventListener('click', handler)
-			},
-      unbind: function(el, binding) {
-        // Remove Event Listeners
-        document.removeEventListener('click', el.__vueClickOutside__)
-        el.__vueClickOutside__ = null
-      }
     }
+    // outside: function(e) {
+    // 	this.clickOutside += 1
+    //     console.log('clicked outside!')
+    //     document.getElementById("myCart").classList.toggle("show");
+    //   },
+    //   inside: function(e) {
+    //   this.clickInside += 1
+    //     console.log('clicked inside!')
+    //   }
   }
+  // directives: {
+  //   'click-outside': {
+  //     bind: function(el, binding, vNode) {
+  //       // Provided expression must evaluate to a function.
+  //       if (typeof binding.value !== 'function') {
+  //       	const compName = vNode.context.name
+  //         let warn = `[Vue-click-outside:] provided expression '${binding.expression}' is not a function, but has to be`
+  //         if (compName) { warn += `Found in component '${compName}'` }
+  //         console.warn(warn)
+  //       }
+  //       // Define Handler and cache it on the element
+  //       const bubble = binding.modifiers.bubble
+  //       const handler = (e) => {
+  //         if (bubble || (!el.contains(e.target) && el !== e.target)) {
+  //         	binding.value(e)
+  //         }
+  //       }
+  //       el.__vueClickOutside__ = handler
+  //       // add Event Listeners
+  //       document.addEventListener('click', handler)
+	// 		},
+  //     unbind: function(el, binding) {
+  //       // Remove Event Listeners
+  //       document.removeEventListener('click', el.__vueClickOutside__)
+  //       el.__vueClickOutside__ = null
+  //     }
+  //   }
+  // }
 }
 </script>
 
